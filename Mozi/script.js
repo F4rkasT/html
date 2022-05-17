acd8f61c6bd4e8e3af4de0a14a72304fe8a5017a
F4rkasT <90308588+F4rkasT@users.noreply.github.com>
2022-05-17 15:28:33 +0200

    Add files via upload

---
+++ Mozi/script.js
@@ -0,0 +1,4 @@
+$('.mozi-szék .szék').on('click', function() {
+  $(this).toggleClass('active');
+  alert("A székedet lefoglaltuk de ha ismét rányomtál a lefoglalt székre abban az esetben töröltük")
+});
